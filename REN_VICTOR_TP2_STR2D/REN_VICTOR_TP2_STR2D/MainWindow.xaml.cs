f4c2ae65657f62a4477fa2d73908b8de2fbf32fa
Vicren77 <46035206+Vicren77@users.noreply.github.com>
2021-12-17 13:03:22 +0100

    Add files via upload

---
+++ REN_VICTOR_TP2_STR2D/REN_VICTOR_TP2_STR2D/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.IO.Packaging;
+using System.Windows;
+
+namespace WpfApplication1
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // the "??" operator checks for nullability and value all at once.
+            // because ConvertCheckBox.IsChecked is of type __bool ?__ which
+            // is a nullable boolean, so it can either be true, false or null
+            var toDecrypt = ConvertCheckBox.IsChecked ?? false;
+            var inputText = InputTextBox.Text;
+            var encryptionmethod = EncryptionComboBox.Text;
+
+            if (toDecrypt)
+            {
+                OutputTextBox.Text = $"{inputText} is gibberish and should be decrypted using {encryptionmethod}";
+            }
+            else
+            {
+                OutputTextBox.Text = $"{inputText} was written as an input to be encrypted using {encryptionmethod}";
+            }
+
+            if (encryptionmethod == "Caesar")
+            {
+                OutputTextBox.Text = Caesar.Code(inputText, toDecrypt);
+                ErrorTextBox.Text = Caesar.Code(inputText, toDecrypt);
+                PopupText.Text = Caesar.Code(inputText, toDecrypt);
+            }
+        }
+    }
+
+    // This class is not instantiated because it is static. 
+    // You might not be able to do this so easily...
+    // And each class should have its own file !
+    internal static class Caesar
+    {
+        public static string Code(string inputText, bool toDecrypt)
+        {
+            // Ternary operator - Google it
+            return toDecrypt ? Decrypt(inputText) : Encrypt(inputText);
+        }
+
+        private static string Encrypt(string inputText)
+        {
+            return $"{inputText} encrypted with Caesar !";
+        }
+
+        private static string Decrypt(string inputText)
+        {
+            return $"{inputText} was decrypted with Caesar !";
+        }
+    }
+}
